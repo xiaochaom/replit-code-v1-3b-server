cedc6cfb6f8b4cd21e1bc8f0fadade282bab9a0e
xiaochaom <410000368@qq.com>
2023-05-05 17:20:41 +0800

    stop sh

--- stop.sh
+++ stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pids=$(ps aux | grep -E 'replit-code-v1-3b-server|python|gunicorn.*flask_app' | grep -v grep | awk '{print $2}')
+pids=$(ps aux | grep -E 'replit-code-v1-3b-server.*python.*gunicorn.*flask_app' | grep -v grep | awk '{print $2}')
 if [ -n "$pids" ]; then
     echo "Killing processes: $pids"
     kill $pids
